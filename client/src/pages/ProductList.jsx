7b90bd42ac1d23ee0346b6435c6a1115c5674a67
Parassahu2308 <sahujuparas786@gmail.com>
2023-05-02 17:41:48 +0530

    :lipstick: Login, Register, product page complete

---
+++ client/src/pages/ProductList.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import Announcement from "../components/Announcement";
+import Products from "../components/Products";
+import NewsLetter from "../components/NewsLetter";
+import Footer from "../components/Footer";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+
+const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  margin: 20px;
+  display: flex;
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Select = styled.select`
+  margin-left: 20px;
+  padding: 10px;
+`;
+
+const Option = styled.option``;
+
+const ProductList = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Dresses</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option selected>Newest</Option>
+            <Option>Price (asc)</Option>
+            <Option>Price (dsc)</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <NewsLetter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default ProductList;
